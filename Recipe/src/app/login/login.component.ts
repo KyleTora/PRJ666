c91411f297f71a8ebd97656b5cbcf59331f7d3d2
Kyle Tora <kyletora1@gmail.com>
2020-03-07 15:01:13 -0500

    done changes

--- Recipe/src/app/login/login.component.ts
+++ Recipe/src/app/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+//import { runInThisContext } from 'vm';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  invalidLogin: boolean = false;
+
+  constructor(private formBuilder: FormBuilder,
+              private router: Router) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.required]
+    });
   }
 
+  onSubmit(){
+    console.log(this.loginForm.value);
+  }
 }
