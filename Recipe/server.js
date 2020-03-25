b16e666e6f0ede59a329af7134665263f9e7c2fa
Kyle Tora <kyletora1@gmail.com>
2020-03-24 22:33:20 -0400

    fix

--- Recipe/server.js
+++ Recipe/server.js
@@ -212,7 +212,7 @@ app.post('/newRecipe', function (request, response) {
         //         response.send('Please enter Recipe!');
         // }
         if(instructions ){
-                connection.query("INSERT INTO Ingredient(ingredient_name, recipe_id) VALUES('yeet',6)",  function (error, results, fields) {
+                connection.query("INSERT INTO Ingredients(ingredient_name, recipe_id) VALUES('yeet',6)",  function (error, results, fields) {
                         if (error) {
                                 response.send('Incorrect Ingredient Format!');
                         } else {
